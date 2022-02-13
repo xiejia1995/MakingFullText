4f8f23c2e4575938393d41b96251b63c92ce913d
xiejia1995 <rg194@uni-heidelberg.de>
2022-02-13 16:13:43 +0100

    Delete import re.py

--- data/Nongjianv_Baishitong/import re.py
+++
@@ -1,6 +0,0 @@
-import re
-str="TextBox[0](+padding)[score(0.765240),[x: 1145, y: 322], [x: 1405, y: 336], [x: 1317, y: 1952], [x: 1057, y: 1938]]"
-
-x=re.findall(r'[)],[[]x: (\d+),',str)
-y=re.findall(r'y: (\d+)[]]',str[40:58])
-print (x,y)
